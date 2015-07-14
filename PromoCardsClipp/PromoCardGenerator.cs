7b3470f96dafa5510fd5ace230bb5b4b44b19497
Gustavo Gambarini <gambarini@gmail.com>
2015-07-14 20:19:00 +1000

    Initial commit

---
+++ PromoCardsClipp/PromoCardGenerator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace PromoCardsClipp
+{
+	public class PromoCardGenerator
+	{
+		private string _privateKey;
+
+		private int _size;
+
+		public PromoCardGenerator (string privateKey, int size)
+		{
+			if (privateKey.Length < 10)
+				throw new Exception ("privateKey must have at least 10 characters");
+
+			_size = size;
+			_privateKey = privateKey;
+
+		}
+
+		public int ExtractNumber (string code)
+		{
+			char[] flipedKey = FlipKey (_privateKey.ToArray());
+
+			char[] decrypted = code.ToArray ().Select (c => {
+
+				int index = new string (flipedKey).IndexOf (c);
+
+				flipedKey = FlipKey (flipedKey);
+
+				return char.Parse(index.ToString());
+			}).ToArray();
+				
+			return int.Parse(new string(decrypted));
+		}
+
+		public string GenerateCode (int number)
+		{
+			string data = number.ToString ().PadLeft (_size, '0');
+			char[] flipedKey = FlipKey (_privateKey.ToArray());
+
+			char[] encrypted = data.ToArray ().Select (c => {
+
+				int i = int.Parse(c.ToString());
+
+				char toCode = GetCharFromKey (i, flipedKey);
+
+				flipedKey = FlipKey(flipedKey);
+
+				return toCode;
+					
+			}).ToArray();
+
+			return new string (encrypted);
+		}
+
+		private char GetCharFromKey(int index, char[] flipedKey){
+			return flipedKey [index];
+		}
+
+		private char[] FlipKey(char[] toFlip){
+			char first = toFlip[0];
+
+			char[] _flipedKey = new char[toFlip.Length];
+
+			_flipedKey [toFlip.Length - 1] = first;
+
+			for (int i = 0; i < toFlip.Length - 1; i++)
+				_flipedKey [i] = toFlip [i + 1];
+
+			return _flipedKey;
+		}
+	}
+}
+
---
+++ PromoCardsClipp/PromoCardGeneratorTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace PromoCardsClipp
+{
+	[TestFixture]
+	public class PromoCardGeneratorTest
+	{
+		const string testKey1 = "QWERTY1234";
+		const string testKey2 = "P5@SA$7DFU8JH";
+		const int number = 100;
+		const string code = "WERYY1";
+
+        [Test]	
+		public void NumberToCodeTest(){
+
+			var promoGen = new PromoCardGenerator (testKey1, 6);
+
+			string codeGenerated = promoGen.GenerateCode (number);
+
+			Assert.AreEqual (code, codeGenerated);
+		}
+
+		[Test]	
+		public void CodeToNumberTest(){
+
+			var promoGen = new PromoCardGenerator (testKey1, 6);
+
+			int numberFromCode = promoGen.ExtractNumber (code);
+
+			Assert.AreEqual (number, numberFromCode);
+		}
+
+		[Test]
+		public void RangeTest(){
+
+			var promoGen = new PromoCardGenerator (testKey2, 6);
+
+			for (int i = 0; i <= 100000; i++){
+				var code = promoGen.GenerateCode (i);
+				var number = promoGen.ExtractNumber (code);
+				System.Diagnostics.Debug.WriteLine (i + " - " + code);
+				Assert.AreEqual (i, number);
+
+			}
+		}
+	}
+}
+
